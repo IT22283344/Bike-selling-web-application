f91f95e6a585120efbb2fcdfd8fc13f754ce7875
Hirusha_R <it22283344@my.sliit.lk>
2024-07-23 19:10:16 +0530

    api work

--- api/index.js
+++ api/index.js
@@ -1,7 +1,40 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import path from 'path';
+dotenv.config();
 
-const app = express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+    console.log(err);
+}); 
+
+
+const __dirname = path.resolve();
+
+const app = express(); 
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000,() => {
     console.log('Server is running on port 3000!');
+});
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+})
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message||'Internet server error' ;
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+
+    });
 });
